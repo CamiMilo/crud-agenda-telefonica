e8bc5b5a18af0643248862bbce9f77cece0de2ae
camilo <cs9886708@gmail.com>
2024-09-11 19:55:01 -0500

    v1.1

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-$host = "localhost";  // Cambia esto si es necesario
+$host = "fra1.clusters.zeabur.com";  // Cambia esto si es necesario
 $user = "root";       // Usuario de MySQL
-$pass = "";           // Contraseña de MySQL
+$pass = "dHbG17Z8ohVE2C0RO6eK3SY49MDi5jnc";           // Contraseña de MySQL
 $db = "agenda_db";    // Nombre de la base de datos
 
 $conn = new mysqli($host, $user, $pass, $db);
