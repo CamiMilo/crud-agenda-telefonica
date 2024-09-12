6686744f30aa577c32c11890447c31ce1d763457
camilo <cs9886708@gmail.com>
2024-09-11 20:06:45 -0500

    v1.3

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $host = 'fra1.clusters.zeabur.com';  // Host del servidor de base de datos
 $port = '30603';                     // Puerto de la base de datos
 $user = 'root';                      // Usuario de la base de datos
 $pass = 'dHbG17Z8ohVE2C0RO6eK3SY49MDi5jnc';  // Contraseña del usuario
-$db = 'zeabur';                      // Nombre de la base de datos
+$db = 'agenda_db';                      // Nombre de la base de datos
 
 // Crear la conexión
 $conn = new mysqli($host, $user, $pass, $db, $port);
